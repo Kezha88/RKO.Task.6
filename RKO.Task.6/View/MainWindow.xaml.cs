ea5a05e6df4ee6b7c08d6e5a1659ea5de5011b3e
Kezha88 <kuku2304@gmail.com>
2023-03-21 03:47:01 +0300

    Добавьте файлы проекта.

---
+++ RKO.Task.6/View/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using RKO.Task._6.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RKO.Task._6.View.Pages.TaskPages;
+using RKO.Task._6.View.Pages;
+
+namespace RKO.Task._6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CoreNavigate.MyConnection = MainFrame;
+            MainFrame.Navigate(new MainPage());
+        }
+        private void BtnTask101_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+       
+        private void BtnTask0_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyConnection?.Navigate(new MainPage());
+        }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+}
